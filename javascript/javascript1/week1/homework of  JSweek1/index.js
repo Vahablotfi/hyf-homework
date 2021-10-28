b1ceedf8ec4de9694ab6e9d74d114390a088d502
HYF M05 <hyf@HYFM05-MacBook-Air.local>
2021-10-28 14:27:14 +0200

    Goodboy-Oldboy adde

--- javascript/javascript1/week1/homework of  JSweek1/index.js
+++ javascript/javascript1/week1/homework of  JSweek1/index.js
@@ -1,4 +1,18 @@
+//Age-ify ( A future age calculator)
 const yearOfBirth = 1991;
 const yearFuture = 2063;
 const age = yearFuture - yearOfBirth ;
 console.log("You will be " + age + " years old in " + yearFuture)
+
+//Goodboy-Oldboy(a DOG AGE CALCULATOR)
+const dogYearOfBirth = 2017;
+const  dogYearFuture = 2027;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = dogYearFuture - dogYearOfBirth;
+if(shouldShowResultInDogYears != 70){
+    console.log("Your dog will be ",dogYear," human years old in",dogYearFuture,".")
+} else {
+    console.log("Your dog will be",dogYear,"dog years old in",dogYearFuture )
+}
+
+
