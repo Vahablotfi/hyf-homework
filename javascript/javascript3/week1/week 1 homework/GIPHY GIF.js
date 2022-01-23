0ebe5f7b48691d299de39e55adeeea6921bb345a
HYF M05 <hyf@HYFM05-MacBook-Air.local>
2022-01-23 12:15:57 +0100

    giphy added

--- javascript/javascript3/week1/week 1 homework/GIPHY GIF.js
+++ javascript/javascript3/week1/week 1 homework/GIPHY GIF.js
@@ -1,4 +1,5 @@
 //making a gif search engin using GIPHY API
+
 const apiKey='1GnflBcS5GtXEQWimLiAfFXZ5O3Y0rHV'
 const gifSearch= document.querySelector(".gif-search")
 const limit= document.querySelector(".limit")
@@ -6,15 +7,18 @@ const btn= document.querySelector("button")
 const img= document.querySelector(".img")
 
 btn.addEventListener('click',(e)=>{
+  e.preventDefault();
   const rqsGif=gifSearch.value;
   const gifNumbers=limit.value;
-  fetch('https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${gifSearch}&limit=${toNum}')
+  const toNumber = parseInt(gifNumbers)
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=1GnflBcS5GtXEQWimLiAfFXZ5O3Y0rHV&q=${rqsGif}&limit=${toNumber}`)
   .then(response=> response.json())
-  .then(for (let i = 0; i < content.data.length; i++) {
-      const gifImage=document.createElement("img")
-      gifImage.src=content.data[i].images.downsized.url
-      img.appendChild(gifImage)
-      
-  }
-  )
-})
+  .then((content)=>{  
+    console.log(content);
+    for (let i = 0; i < content.data.length; i++) {
+    const gifImage=document.createElement("img")
+    gifImage.src=content.data[i].images.downsized.url;
+    img.appendChild(gifImage);
+    
+}}
+  )})
