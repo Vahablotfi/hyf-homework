8598bebcb98968d760e000066814d152529bd1d7
Vahablotfi <vahablotfi1@gmail.com>
2022-04-16 00:47:58 +0200

    formatted versin

--- react/react1/week3/react1-week-homework3/src/UserStory/UserStory.js
+++ react/react1/week3/react1-week-homework3/src/UserStory/UserStory.js
@@ -19,68 +19,61 @@ false again with clicking on close button inside the popup component.
 there will be more explanation inside each component file.
 
 */
-import React from 'react'
-import './UserStory.css'
-import  { useEffect, useState } from 'react';
-import SubmitForms from './SubmitForms'
-import ShiftOverview from './ShiftOverview'
-import Popup from './Popup';
-
-
-
-
+import React from "react";
+import "./UserStory.css";
+import { useEffect, useState } from "react";
+import SubmitForms from "./SubmitForms";
+import ShiftOverview from "./ShiftOverview";
+import Popup from "./Popup";
 
 function UserStory() {
-  const [shifts, setShifts] = useState([])
+  const [shifts, setShifts] = useState([]);
   const [employeeButton, setEmployeeButton] = useState(false);
   const [employerButton, setEmployerButton] = useState(false);
- 
-  
-       useEffect(() => {
-      const getShifts = async () => {
-      const savedShifts = await fetchShifts()
-       setShifts(savedShifts)
-     }
-    getShifts()
-       }, [])
-  
-      const fetchShifts = async () => {
-      const res = await fetch(' https://gist.githubusercontent.com/benna100/5fd674171ea528d7cd1d504e9bb0ca6f/raw')
-       const data = await res.json()
-         return data
-     }
-  
-    const addShifts = (shift) => {
-      setShifts([...shifts, shift])       
-    }
-   
-    return (
-      <div >
-        
-          <div className='emp-btn'>
-          <button className='popup-btn' onClick={() => setEmployeeButton(true)} >Employee</button>
-           <button className='popup-btn' onClick={()=>setEmployerButton(true)} >Employer</button>
-        </div>
-        
-        <Popup trigger={employeeButton} setTrigger={setEmployeeButton} >
-         <div className='submitContainer'>
+
+  useEffect(() => {
+    const getShifts = async () => {
+      const savedShifts = await fetchShifts();
+      setShifts(savedShifts);
+    };
+    getShifts();
+  }, []);
+
+  const fetchShifts = async () => {
+    const res = await fetch(
+      " https://gist.githubusercontent.com/benna100/5fd674171ea528d7cd1d504e9bb0ca6f/raw"
+    );
+    const data = await res.json();
+    return data;
+  };
+
+  const addShifts = (shift) => {
+    setShifts([...shifts, shift]);
+  };
+
+  return (
+    <div>
+      <div className="emp-btn">
+        <button className="popup-btn" onClick={() => setEmployeeButton(true)}>
+          Employee
+        </button>
+        <button className="popup-btn" onClick={() => setEmployerButton(true)}>
+          Employer
+        </button>
+      </div>
+
+      <Popup trigger={employeeButton} setTrigger={setEmployeeButton}>
+        <div className="submitContainer">
           <SubmitForms onAdd={addShifts}></SubmitForms>
         </div>
-        </Popup>
-
-
-           <div >
-                  
+      </Popup>
+      <Popup trigger={employerButton} setTrigger={setEmployerButton}>
+        <div className="shift-Overview container">
+          <ShiftOverview shifts={shifts}></ShiftOverview>
         </div>
-        
-
-        <Popup trigger={employerButton} setTrigger={setEmployerButton}>
-        <div className="shift-Overview container"> 
-         <ShiftOverview shifts={shifts}></ShiftOverview>
-        </div>
-        </Popup>
+      </Popup>
     </div>
-  )
+  );
 }
 
 export default UserStory;
