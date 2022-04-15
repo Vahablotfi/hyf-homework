84aff932a7278eb91d4bbe3a983b4a8e3a3cc948
Vahablotfi <vahablotfi1@gmail.com>
2022-04-16 00:47:24 +0200

    formatted versin

---
+++ react/react1/week3/react1-week-homework3/src/App.js
@@ -0,0 +1,14 @@
+import UpToDo from "../../react1-week-homework3/src/ToDoList/UpToDo";
+import UserStory from "../../react1-week-homework3/src/UserStory/UserStory";
+
+function App() {
+  return (
+    <div>
+      <UserStory></UserStory>
+
+      <UpToDo></UpToDo>
+    </div>
+  );
+}
+
+export default App;
