c893895bc8cd4ddda4f9bf8f5d016b2add309bcf
Vahablotfi <vahablotfi1@gmail.com>
2022-04-16 20:45:56 +0200

    latest edit

--- nodejs/week3/week3/warmup/index.js
+++ nodejs/week3/week3/warmup/index.js
@@ -1,99 +1,66 @@
+const { query } = require( "express" );
 const express = require("express");
 const app = express();
+const typeValidation=require("./validator")
 app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-
-app.get("/", (req, res) => res.send("nodejs week3 homework"));
+app.use( express.json() );
 
 
-app.get("/calculator/multiply", (req, res) => {
-   
-    const firstNumber= Number(req.query.firstParam);
-    const secondParam=req.query.secondParam;
-   
-    
-    if(typeof(secondParam) === 'object'){
-       
-        const secondParamArray=secondParam.reduce( function (previous,current) {
-         return Number(previous)*Number(current)});
-        
-         const result1=firstNumber * secondParamArray;
-        res.json(`The answer for your request is ${result1}`);
-    }else{
-        const secondNumber=Number(req.query.secondParam);
-        const result =firstNumber * secondNumber;
-        res.send(`The answer for your request is ${result}`);
-    }   
-   
+
+app.get( "/", ( req, res ) => {
+    res.send( "nodejs week3 homework" )
+} );
+
+
+app.get("/calculator/multiply", typeValidation, async (req, res) => {
+
+
+  if (Object.keys(req.query).length !== 0) {
+    const values = Object.values(req.query);
+    const result = values.reduce(
+      (previous, current) => Number(previous) * Number(current)
+    );
+
+    res.send(`The answer for your request is ${result}`);
+  }
+ 
 });
 
-app.get("/calculator/add", (req, res) => {
-   
-    const firstNumber= Number(req.query.firstParam);
-    const secondNumber=Number(req.query.secondParam);
-    const thirdNumber =Number(req.query.thirdParam);
-    if(thirdNumber){
-        const result1=firstNumber + secondNumber + thirdNumber;
-        res.json(`The answer for your request is ${result1}`);
-    }else{
-        const result =firstNumber + secondNumber;
-    res.json(`The answer for your request is ${result}`);
-    }
+app.get("/calculator/add", typeValidation,async (req, res) => {
+ 
+  if (Object.keys(req.query).length !== 0) {
+    const values = Object.values(req.query);
+    const result = values.reduce(
+      (previous, current) => Number(previous) + Number(current)
+    );
 
+    res.send(`The answer for your request is ${result}`);
+  }
 });
 
-app.get("/calculator/division", (req, res) => {
-    
-    const firstNumber= Number(req.query.firstParam);
-    const secondNumber=Number(req.query.secondParam);
-    const result =firstNumber / secondNumber;
-    res.json(`The answer for your request is ${result}`);
-    
+app.get("/calculator/division", typeValidation, (req, res) => {
+
+  if (Object.keys(req.query).length !== 0) {
+    const values = Object.values(req.query);
+    const result = values.reduce(
+      (previous, current) => Number(previous) / Number(current)
+    );
+
+    res.send(`The answer for your request is ${result}`);
+  }
 });
 
-app.get("/calculator/Subtraction", (req, res) => {
-    
-    const firstNumber= Number(req.query.firstParam);
-    const secondNumber=Number(req.query.secondParam);
-    const result =firstNumber - secondNumber;
-    res.json(`The answer for your request is ${result}`);
-    
+app.get("/calculator/Subtraction", typeValidation, (req, res) => {
+  if (Object.keys(req.query).length !== 0) {
+    const values = Object.values(req.query);
+    const result = values.reduce(
+      (previous, current) => Number(previous) - Number(current)
+    );
+
+    res.send(`The answer for your request is ${result}`);
+  }
 });
 
-app.post("/calculator/multiply", (req, res) => {
-      
-    const firstNumber= Number(req.body.firstParam);
-    const secondNumber=Number(req.body.secondParam);
-    const result = firstNumber * secondNumber;
-    res.json(`The answer for your request is ${result}`);
-   
-});
 
-app.post("/calculator/add", (req, res) => {
-    
-    const firstNumber= Number(req.body.firstParam);
-    const secondNumber=Number(req.body.secondParam);
-    const result =firstNumber + secondNumber;
-    res.json(`The answer for your request is ${result}`);
-   
-});
-
-app.post("/calculator/division", (req, res) => {
-    
-    const firstNumber= Number(req.body.firstParam);
-    const secondNumber=Number(req.body.secondParam);
-    const result =firstNumber / secondNumber;
-    res.json(`The answer for your request is ${result}`);
-  
-});
-
-app.post("/calculator/Subtraction", (req, res) => {
-    
-    const firstNumber= Number(req.body.firstParam);
-    const secondNumber=Number(req.body.secondParam);
-    const result =firstNumber - secondNumber;
-    res.json(`The answer for your request is ${result}`);
-   
-});
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
